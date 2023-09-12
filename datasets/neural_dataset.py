5db0d824ad6319dda61b181b8bca525255be1f4c
neutralpronoun <alexowendavies@gmail.com>
2023-09-12 13:05:19 +0100

    ESWR now employs random exploration samplers

--- datasets/neural_dataset.py
+++ datasets/neural_dataset.py
@@ -102,22 +102,22 @@ def add_attrs_to_graph(g):
 
 def ESWR(graph, n_graphs, size):
 
-    possible_samplers = inspect.getmembers(samplers, inspect.isclass)
-
-    possible_samplers = [item[1] for item in possible_samplers]
-    possible_samplers = [MetropolisHastingsRandomWalkSampler, DiffusionSampler, DepthFirstSearchSampler]
-    # selected_sampler = possible_samplers[np.random.randint(len(possible_samplers))]
-
-
-    print(f"Sampling {n_graphs} graphs from {graph}")
-    graphs = []
-    for i in tqdm(range(n_graphs), leave = False):
-        selected_sampler = possible_samplers[np.random.randint(len(possible_samplers))]
-        sampler = selected_sampler(number_of_nodes=np.random.randint(12, 48))
-        graphs.append(nx.convert_node_labels_to_integers(sampler.sample(graph)))
+    # possible_samplers = inspect.getmembers(samplers, inspect.isclass)
+    #
+    # possible_samplers = [item[1] for item in possible_samplers]
+    # possible_samplers = [MetropolisHastingsRandomWalkSampler, DiffusionSampler, DepthFirstSearchSampler]
+    # # selected_sampler = possible_samplers[np.random.randint(len(possible_samplers))]
+    #
+    #
+    # print(f"Sampling {n_graphs} graphs from {graph}")
+    # graphs = []
+    # for i in tqdm(range(n_graphs), leave = False):
+    #     selected_sampler = possible_samplers[np.random.randint(len(possible_samplers))]
+    #     sampler = selected_sampler(number_of_nodes=np.random.randint(12, 48))
+    #     graphs.append(nx.convert_node_labels_to_integers(sampler.sample(graph)))
     # sampler = selected_sampler(number_of_nodes=np.random.randint(12, 36))
-    # sampler = MetropolisHastingsRandomWalkSampler(number_of_nodes=np.random.randint(12, 48))
-    graphs = [nx.convert_node_labels_to_integers(graph) for i in tqdm(range(n_graphs))]
+    sampler = ForestFireSampler(48)
+    graphs = [nx.convert_node_labels_to_integers(sampler.sample(graph)) for i in tqdm(range(n_graphs))]
 
     return graphs
 
