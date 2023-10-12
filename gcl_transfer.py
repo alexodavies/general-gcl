8c9ea853c085dce9ebb1d161dc467192ed166090
neutralpronoun <alexowendavies@gmail.com>
2023-10-12 10:53:36 +0100

    ESWR now employs random exploration samplers

--- gcl_transfer.py
+++ gcl_transfer.py
@@ -480,7 +480,7 @@ def arg_parse():
                         help='batch size')
     parser.add_argument('--drop_ratio', type=float, default=0.2,
                         help='Dropout Ratio / Probability')
-    parser.add_argument('--epochs', type=int, default=10,
+    parser.add_argument('--epochs', type=int, default=32,
                         help='Train Epochs')
     parser.add_argument('--reg_lambda', type=float, default=5.0, help='View Learner Edge Perturb Regularization Strength')
 
