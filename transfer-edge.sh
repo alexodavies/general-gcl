e0aea78aa6dd7be23b59e5a49ee1cd3fb7e81512
neutralpronoun <alexowendavies@gmail.com>
2024-01-09 15:59:31 +0000

    Bash updates

--- transfer-edge.sh
+++ transfer-edge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for checkpoint_name in edge-views-chem.pt edge-views-social.pt
+for checkpoint_name in edge-views-all.pt edge-views-chem.pt edge-views-social.pt
 do
     python transfer.py --checkpoint "$checkpoint_name"
 done
