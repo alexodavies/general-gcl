0444f1e50ea34491794a113028617b826c2a9d7c
neutralpronoun <alexowendavies@gmail.com>
2024-04-11 12:44:10 +0100

    Script to run all node classification transfers

---
+++ transfer-all-node-classification-full-features.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt
+do
+    python node_classification_transfer.py --checkpoint "$checkpoint_name" -f --num_epochs 10 --backbone gin
+done
+
+for checkpoint_name in  untrained gat-all.pt gat-chem.pt gat-social.pt
+do
+    python node_classification_transfer.py --checkpoint "$checkpoint_name" -f --num_epochs 10 --backbone gat
+done
+
+
+for checkpoint_name in  untrained gcn-all.pt gcn-chem.pt gcn-social.pt
+do
+    python node_classification_transfer.py --checkpoint "$checkpoint_name" -f --num_epochs 10 --backbone gcn
+done
