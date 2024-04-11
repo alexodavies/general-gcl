0b2c22865151bdaf82ebd35da73c2da5fca06a39
neutralpronoun <alexowendavies@gmail.com>
2024-04-11 12:59:12 +0100

    Script to run all node classification transfers

--- transfer-all-full-features.sh
+++ transfer-all-full-features.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt
+for checkpoint_name in  all-100.pt chem-100.pt social-100.pt
 do
     python features_transfer.py --checkpoint "$checkpoint_name" -f --num 5000000 --backbone gin
 done
