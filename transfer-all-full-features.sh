225b4db40f3cf69ad98da84ad9cde70206f774e0
neutralpronoun <alexowendavies@gmail.com>
2024-04-15 10:44:03 +0100

    Script for transfer on node and edge prediction without features.

--- transfer-all-full-features.sh
+++ transfer-all-full-features.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt
+for checkpoint_name in  untrained chem-100.pt social-100.pt all-100.pt
+do
+    python features_transfer.py --checkpoint "$checkpoint_name" -f --num 5000000 --backbone gin --batch_size 512
+done
+#
+#for checkpoint_name in  gat-all.pt gat-chem.pt gat-social.pt
 #do
-#    python features_transfer.py --checkpoint "$checkpoint_name" -f --num 5000000 --backbone gin --batch_size 512
+#    python features_transfer.py --checkpoint "$checkpoint_name" -f --num 5000000 --backbone gat
 #done
-
-for checkpoint_name in  gat-all.pt gat-chem.pt gat-social.pt
-do
-    python features_transfer.py --checkpoint "$checkpoint_name" -f --num 5000000 --backbone gat
-done
-
-
-for checkpoint_name in  untrained gcn-all.pt gcn-chem.pt gcn-social.pt
-do
-    python features_transfer.py --checkpoint "$checkpoint_name" -f --num 5000000 --backbone gcn
-done
+#
+#
+#for checkpoint_name in  untrained gcn-all.pt gcn-chem.pt gcn-social.pt
+#do
+#    python features_transfer.py --checkpoint "$checkpoint_name" -f --num 5000000 --backbone gcn
+#done
--- transfer-all-node-classification.sh
+++ transfer-all-node-classification.sh
@@ -2,18 +2,20 @@
 
 
 
-for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt
+for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt edge-views-all.pt
 do
     python node_classification_transfer.py --checkpoint "$checkpoint_name" --epochs 10 --backbone gin
 done
 
-for checkpoint_name in  untrained gat-all.pt gat-chem.pt gat-social.pt
-do
-    python node_classification_transfer.py --checkpoint "$checkpoint_name" --epochs 10 --backbone gat
-done
+python node_classification_transfer.py --checkpoint edge-views-all.pt --epochs 10 --backbone gin -f
 
-
-for checkpoint_name in  untrained gcn-all-shallow.pt gcn-all.pt gcn-chem.pt gcn-social.pt
-do
-    python node_classification_transfer.py --checkpoint "$checkpoint_name" --epochs 10 --backbone gcn
-done
+#for checkpoint_name in  untrained gat-all.pt gat-chem.pt gat-social.pt
+#do
+#    python node_classification_transfer.py --checkpoint "$checkpoint_name" --epochs 10 --backbone gat
+#done
+#
+#
+#for checkpoint_name in  untrained gcn-all-shallow.pt gcn-all.pt gcn-chem.pt gcn-social.pt
+#do
+#    python node_classification_transfer.py --checkpoint "$checkpoint_name" --epochs 10 --backbone gcn
+#done
