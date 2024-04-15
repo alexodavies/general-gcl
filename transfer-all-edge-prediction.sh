511d145f87d9a1efecd55f99832de2567d2a7537
neutralpronoun <alexowendavies@gmail.com>
2024-04-15 10:57:38 +0100

    Script for transfer on node and edge prediction without features.

--- transfer-all-edge-prediction.sh
+++ transfer-all-edge-prediction.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 
+python edge_prediction_transfer.py --checkpoint untrained --epochs 10 --backbone gin --batch_size 512 -f
 
 for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt edge-views-all.pt
 do
     python edge_prediction_transfer.py --checkpoint "$checkpoint_name" --epochs 10 --backbone gin --batch_size 512
 done
 
-for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt edge-views-all.pt
-do
-    python edge_prediction_transfer.py --checkpoint "$checkpoint_name" --epochs 10 --backbone gin --batch_size 512 -f
-done
+python edge_prediction_transfer.py --checkpoint edge-views-all.pt --epochs 10 --backbone gin --batch_size 512 -f
+
+#for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt edge-views-all.pt
+#do
+#    python edge_prediction_transfer.py --checkpoint untrained --epochs 10 --backbone gin --batch_size 512 -f
+#done
 
 
 #for checkpoint_name in  untrained gat-all.pt gat-chem.pt gat-social.pt
--- transfer-all-node-classification.sh
+++ transfer-all-node-classification.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 
-#python node_classification_transfer.py --checkpoint untrained --epochs 10 --backbone gin -f
+python node_classification_transfer.py --checkpoint untrained --epochs 10 --backbone gin -f
 
 for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt edge-views-all.pt
 do
     python node_classification_transfer.py --checkpoint "$checkpoint_name" --epochs 10 --backbone gin  --batch_size 512
 done
 
-for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt edge-views-all.pt
-do
-    python node_classification_transfer.py --checkpoint "$checkpoint_name" --epochs 10 --backbone gin -f  --batch_size 512
-done
+#for checkpoint_name in  untrained all-100.pt chem-100.pt social-100.pt edge-views-all.pt
+#do
+python node_classification_transfer.py --checkpoint edge-views-all.pt --epochs 10 --backbone gin -f  --batch_size 512
+#done
 
 #for checkpoint_name in  untrained gat-all.pt gat-chem.pt gat-social.pt
 #do
