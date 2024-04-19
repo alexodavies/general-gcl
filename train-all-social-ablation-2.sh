a8524c6d8c1f7c39716988655b347793a7184ed7
neutralpronoun <alexowendavies@gmail.com>
2024-04-19 12:01:38 +0100

    Splitting ablation into two training sessions

---
+++ train-all-social-ablation-2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#python train.py --epochs 100 --backbone gin -c --exclude facebook_large
+#python train.py --epochs 100 --backbone gin -c --exclude twitch_egos
+#python train.py --epochs 100 --backbone gin -c --exclude cora
+python train.py --epochs 100 --backbone gin -c --exclude roads
+python train.py --epochs 100 --backbone gin -c --exclude fruit_fly
--- train-all-social-ablation.sh
+++ train-all-social-ablation.sh
@@ -3,5 +3,5 @@
 python train.py --epochs 100 --backbone gin -c --exclude facebook_large
 python train.py --epochs 100 --backbone gin -c --exclude twitch_egos
 python train.py --epochs 100 --backbone gin -c --exclude cora
-python train.py --epochs 100 --backbone gin -c --exclude roads
-python train.py --epochs 100 --backbone gin -c --exclude fruit_fly
+#python train.py --epochs 100 --backbone gin -c --exclude roads
+#python train.py --epochs 100 --backbone gin -c --exclude fruit_fly
