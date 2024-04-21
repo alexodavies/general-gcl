c1dcd42312458b3fbe830676345486f2dc158440
neutralpronoun <alexowendavies@gmail.com>
2024-04-21 16:32:24 +0100

    Cheeky bit of reformating + script for ablation transfer

---
+++ scripts/transfer-ablation-models-features.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for checkpoint_name in cora-excluded.pt facebook-excluded.pt fly-excluded.pt twitch-excluded.pt twitch-excluded.pt
+do
+    python transfer.py --checkpoint "$checkpoint_name"  --num 5000000 --batch_size 512 --epochs 50
+
+done
+#
+#for checkpoint_name in  gat-all.pt gat-chem.pt gat-social.pt
+#do
+#    python features_transfer.py --checkpoint "$checkpoint_name" -f --num 5000000 --backbone gat
+#done
+#
+#
+#for checkpoint_name in  untrained gcn-all.pt gcn-chem.pt gcn-social.pt
+#do
+#    python features_transfer.py --checkpoint "$checkpoint_name" -f --num 5000000 --backbone gcn
+#done
