bbcc71df93c7527403910d1b05d57c7bd18c4ce1
neutralpronoun <alexowendavies@gmail.com>
2024-10-17 10:27:50 +0100

    Trimming output size

--- LLMs/llm.py
+++ LLMs/llm.py
@@ -43,7 +43,7 @@ class LLM:
 
     def generate_text(self, user_prompt, system_prompt="You are a helpful assistant."):
         print("generating text")
-        completion = self.pipe(user_prompt, max_length = 5000, return_full_text = False)
+        completion = self.pipe(user_prompt, max_length = 50, return_full_text = False)
         return completion# [0]["generated_text"]
 
     def forward(self, graph):
